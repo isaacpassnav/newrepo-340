5f8b8582a022e909c7923782266b121e2f7fcb22
Your Name <pasapera279@gmail.com>
2025-11-18 17:38:40 -0500

    week03 completed, improve to controllers and styles for nav

--- routes/errorRoute.js
+++ routes/errorRoute.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// Ruta que genera un error intencional
 router.get("/trigger-error", (req, res, next) => {
   // Genera un error intencional
   const error = new Error("This is an intentional 500 error for testing!");
